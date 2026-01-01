51d0c3c3813a937dd41b3017e8bd6e5eefb7a7e8
miladinovicem <miladinovicmilica2002@gmail.com>
2026-01-02 00:11:49 +0100

    add auth service for user registration

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<User?> registerWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return result.user;
+    } on FirebaseAuthException catch (e) {
+      throw e.message ?? 'Registration failed';
+    }
+  }
+}
